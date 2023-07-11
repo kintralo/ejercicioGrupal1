fc5af792ef274dc00a8365135274f766a49593ca
Pablov <juan.vasquez@valposystem.com>
2023-07-11 16:31:22 -0400

    Modificacion en el nombre

--- src/main/java/servlet/servletCapacitacion.java
+++ src/main/java/servlet/servletCapacitacion.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 /**
  * Servlet implementation class servletCapacitacion
  */
-public class servletCapacitacion extends HttpServlet {
+public class ServletCapacitacion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public servletCapacitacion() {
+    public ServletCapacitacion() {
         super();
         // TODO Auto-generated constructor stub
     }
